5d6b7a56f02b12455e33982eda7dce311d278c72
Waga <wagaodongo@gmail.com>
2019-10-05 21:49:55 +0300

    Player Stat Page

--- NBA Players/ContentView.swift
+++ NBA Players/ContentView.swift
@@ -14,8 +14,17 @@ struct ContentView: View {
         VStack {
         Image("gs").resizable().frame(height: 250)
             
-        Image("steph")
+            Image("steph").clipShape(Circle())
+                .background(Circle()
+                .foregroundColor(.white))
+                .overlay(Circle()
+                .stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 15).offset(x: 0, y: -80)
+                .padding(.bottom, -90)
+            
         Text("Steph Curry")
+            .font(.system(size:50))
+            .fontWeight(.bold)
             Spacer()
         }.edgesIgnoringSafeArea(.top)
         
